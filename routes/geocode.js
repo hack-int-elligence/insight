309d9a69a603838af63d7700a001a10d54abb405
Devesh Dayal <deveshd@seas.upenn.edu>
2015-11-07 12:16:33 -0500

    facebook check in built

--- routes/geocode.js
+++ routes/geocode.js
@@ -13,7 +13,10 @@ var hat = require('hat');
 var request = require('request');
 
 var YALE_API_BASE_URL = 'https://gw.its.yale.edu';
-var YALE_API_KEY = '';
+var YALE_API_KEY = 'l7xxd6809c22c97b4f96bb8361a201f71fb3';
+// TEST URL: https://gw.its.yale.edu/soa-gateway/buildings/feed?type=json?apikey=l7xxd6809c22c97b4f96bb8361a201f71fb3
+var FACEBOOK_APP_ID = '674671289335201';
+var FACEBOOK_APP_SECRET = 'a7e1c3c097560ba5ae65015405a1f19e';
 
 var THRESHOLD = 5;
 
@@ -119,36 +122,45 @@ router.get('/', function(req, res) {
 router.post('/fb_checkin', function(req, res) {
     var FB = require('fb');
     FB.setAccessToken(req.body.authToken);
+    var deprecatedFQLQuery = 'SELECT page_id,name,latitude,longitude FROM place WHERE distance(latitude, longitude, ' + req.body.latitude + ', ' + req.body.longitude + ') < 50';
     // first find the closest page location using FQL radius search of 50m
-    FB.api('fql', {
-        q: 'SELECT page_id,name,latitude,longitude FROM place WHERE distance(latitude, longitude, ' + req.body.latitude + ', ' + req.body.longitude + ') < 50'
-    }, function(response) {
-        if (!response || res.error) {
-            console.log(!res ? 'error occurred' : res.error);
-            res.send(res.error);
-        } else {
-            // check to see the place even exists
-            if (response.data && response.data.length > 0) {
-                // use the first place returned
-                var place_details = response.data[0];
-                console.log(placeDetails);
-                var place_id = '';
-                // currently set to private for testing
-                FB.api('me/feed', 'post', {
-                    body: 'I just checked in here!',
-                    place: place_id,
-                    privacy: {
-                        value: 'SELF'
-                    }
-                }, function(checkinResponse) {
-                    console.log(checkinResponse);
-                    res.status(200).send(checkinResponse);
-                });
+    request('https://graph.facebook.com/oauth/access_token?client_id=' + FACEBOOK_APP_ID + '&client_secret=' + FACEBOOK_APP_SECRET + '&grant_type=client_credentials', function(e,r,b) {
+        var app_access_token = b.split('=')[1];
+        FB.setAccessToken(app_access_token);
+        FB.api('/search', {
+            q: req.body.name,
+            type: 'page',
+            center: req.body.latitude + ',' + req.body.longitude,
+            distance: '50'
+        }, function(response) {
+            if (!response || response.error) {
+                console.log(!response ? 'error occurred' : response.error);
+                res.send(response.error);
             } else {
-                // if no page found, send back error
-                res.status(500).send('No location found for those coordinates. Check-in failed.')
+                // check to see the place even exists
+                if (response.data && response.data.length > 0) {
+                    // use the first place returned
+                    var place_details = response.data[0];
+                    console.log(place_details);
+                    var place_id = place_details.id;
+                    // currently set to private for testing
+                    FB.setAccessToken(req.body.authToken);
+                    FB.api('me/feed', 'post', {
+                        body: 'I just checked in at ' + place_details.name,
+                        place: place_id,
+                        privacy: {
+                            value: 'SELF'
+                        }
+                    }, function(checkinResponse) {
+                        console.log(checkinResponse);
+                        res.status(200).send(checkinResponse);
+                    });
+                } else {
+                    // if no page found, send back error
+                    res.status(500).send('No location found for those coordinates. Check-in failed.')
+                }
             }
-        }
+        });
     });
 });
 
